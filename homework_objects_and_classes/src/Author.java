fce5db06bffc43da9f690265583f105d1ad3033e
Taumi62831853071 <113852275+Taumi62831853071@users.noreply.github.com>
2022-10-26 19:41:11 +0300

    Update Author.java

--- homework_objects_and_classes/src/Author.java
+++ homework_objects_and_classes/src/Author.java
@@ -1,6 +1,6 @@
 public class Author {
-    String firstname;
-    String lastname;
+    private String firstname;
+    private String lastname;
 
     public Author(String firstname, String lastname) {
         this.firstname = firstname;
